98a4b3256b1be154b1349be1ee738241f27fd967
amdadulhaque-dev <karnoder1998@gmail.com>
2025-03-25 10:06:36 +0600

    d

---
+++ src/PhphaqueServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+    namespace Phphaque\LaravelPhphaque;
+
+    use Illuminate\Support\ServiceProvider;
+
+    class PhphaqueServiceProvider extends ServiceProvider {
+        
+        public function boot(){
+
+        }
+
+    }
